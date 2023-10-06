9f42078b46f4ebaccccaafecce7a2e05b590a284
Mohanad Khaled Hassan <127995481+MohanadKhh@users.noreply.github.com>
2023-10-07 01:31:19 +0300

    Update LED_interface.h

--- Traffic_Lights/HAL/LED/LED_interface.h
+++ Traffic_Lights/HAL/LED/LED_interface.h
@@ -1,9 +1,9 @@
 /**********************************************************************/
 /* Author               : 	Mohanad Khaled Hassan                     */
 /* Origin Date          : 	05/10/2023                                */
-/* Layer                : 	HAL Layer                 	   		      */
-/* SWC                  : 	LCD                      		          */
-/* Version              : 	1.0.0                          			  */
+/* Layer                : 	HAL Layer                 	   		        */
+/* SWC                  : 	LED                      		              */
+/* Version              : 	1.0.0                          		     	  */
 /**********************************************************************/
 
 #ifndef HAL_LED_LED_INTERFACE_H_
